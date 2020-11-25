7ee02320926dea8582205f55ec5124529d9ec31a
Sean McNamee <seanmcnamee.45@gmail.com>
2020-11-25 00:34:33 -0500

    updated imports

--- src/app/App.java
+++ src/app/App.java
@@ -1,9 +1,5 @@
 package app;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.util.Scanner;
-
 import app.GUI.GUI;
 import app.GUI.Pages.AddPage;
 import app.GUI.Pages.ClosingPage;
@@ -12,15 +8,15 @@ import app.GUI.Pages.MenuPage;
 import app.GUI.Pages.ViewDataMenuPage;
 import app.backend.fileaccess.FileAccess;
 import app.backend.fileaccess.KeyValue;
+import app.backend.tree.LeafNode;
+import app.backend.tree.Tree;
 
 public class App {
 
     public static int MENU = 0, ADD_DATA = 1, VIEW_DATA = 2, DELETE_DATA = 3, CLOSING_DATA = 4;
     
     public static void main(String[] args) throws Exception {
-        GUI g = new GUI(new MenuPage(), new AddPage(), new ViewDataMenuPage(), new DeletePage(), new ClosingPage());
-        System.out.println("Hello Java");
-        g.start();
+        startGUI();
 
 		//testTree();
         //testAppend();
@@ -29,7 +25,7 @@ public class App {
     }
 	
 	private static void startGUI() {
-        GUI g = new GUI(new MenuPage(), new AddDataMenuPage(), new ViewDataMenuPage());
+        GUI g = new GUI(new MenuPage(), new AddPage(), new ViewDataMenuPage(), new DeletePage(), new ClosingPage());
         System.out.println("Hello Java");
         g.start();
     }
